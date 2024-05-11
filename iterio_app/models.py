63bc184abf8939bf1dc3775f0ae6086f8fee9ee8
elizabethmendez <elizabeth.zabala3@gmail.com>
2024-05-11 19:24:33 -0400

    Add services to the home page and some models

--- iterio_app/models.py
+++ iterio_app/models.py
@@ -3,7 +3,19 @@ import datetime
 
 # Create your models here.
 
-class Member(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=100)
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=100)
+
+class ServiceProvider(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    service_type = models.CharField(max_length=100)
+    service_name = models.CharField(max_length=100)
+    service_description = models.CharField(max_length=100)
+
