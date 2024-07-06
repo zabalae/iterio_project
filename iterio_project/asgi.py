7720e4f7c624816faffdfba20d9c0184e431e353
Nicole-Alicea <142937639+Nicole-Alicea@users.noreply.github.com>
2024-07-06 09:43:43 -0400

    Update asgi.py

--- iterio_project/asgi.py
+++ iterio_project/asgi.py
@@ -8,9 +8,23 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'iterio_project.settings')
+django.setup()
 
-application = get_asgi_application()
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from iterio_app.routing import websocket_urlpatterns
+
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'https': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    )
+})
